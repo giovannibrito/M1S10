348a67e14cffb5ff1ad98af821475fa89e6c5360
giovannibrito <giovannileonibrito@bol.com.br>
2022-12-12 14:19:52 -0300

    Commit inicial

---
+++ src/app/components/notification-list/notification-list.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {NOTIFICACOES} from "../../static/notificacoes"
+
+interface Notificacao{
+  aplicativo: string;
+  mensagem: string;
+  data: string;
+  lido: boolean;
+}
+
+@Component({
+  selector: 'ntap-notification-list',
+  templateUrl: './notification-list.component.html',
+  styleUrls: ['./notification-list.component.scss']
+})
+export class NotificationListComponent {
+  notificacoes: Notificacao[] = NOTIFICACOES;
+  
+  marcarLido(notificacao: Notificacao) {
+    notificacao.lido = true
+  }
+}
---
+++ src/app/static/notificacoes.ts
@@ -0,0 +1,8 @@
+export const NOTIFICACOES = 
+[
+{aplicativo: "iFood", mensagem: "Seu pedido chegou!", data: "agora", lido:false},
+{aplicativo: "Nubank ", mensagem: "Sua fatura está fechada", data: "15 minutos atrás", lido:false},
+{aplicativo: "Shopee", mensagem: "Não perca as promoções da Black Friday!", data: "2h atrás", lido:false},
+{aplicativo: "Spotify", mensagem: "Ouça o novo albúm de Natiruts", data: "4h atrás", lido:false},
+{aplicativo: "Duolingo", mensagem: "Faça sua lição e não perca sua ofensiva de 127 dias", data: "1d atrás", lido:false}
+]
